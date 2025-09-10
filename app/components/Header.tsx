819d31016a848e83ac8db1cf6f6810f58fbd30f8
YUKITAKA YAMASHITA <takasnowstyle1821@gmail.com>
2025-09-11 00:23:31 +0900

    Headerをクライアントコンポーネントに変更してuseSessionとsignOutを使用可能に
    - Headerをサーバーコンポーネントからクライアントコンポーネントに変更
    - ログアウトボタンでsignOutをonClickで呼び出せるように対応
    - セッション情報の取得をuseSessionに変更し利用可能に

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -1,34 +1,34 @@
+'use client'
 import Image from "next/image";
 import Link from "next/link";
 import React from "react";
-import { getServerSession } from "next-auth";
-import { nextAuthOptions } from "../lib/next-auth/options";
 import { User } from "../types/types";
+import { signOut, useSession } from "next-auth/react";
 // User型をインポート
 
-const Header = async () => {
+const Header = () => {
   // ------------------
-  // const { data: session } = useSession();
-  // // useSession() が返すオブジェクトの中から data プロパティだけを取り出して
-  // // それを session という名前の変数 に代入しています。
+  const { data: session } = useSession();
+  // useSession() が返すオブジェクトの中から data プロパティだけを取り出して
+  // それを session という名前の変数 に代入しています。
 
-  // // 構造例）
-  // //   {
-  // //     data: { user: { name: "Taro" } },
-  // //     status: "authenticated"
-  // //   }
+  // 構造例）
+  //   {
+  //     data: { user: { name: "Taro" } },
+  //     status: "authenticated"
+  //   }
 
   // console.log(session);
 
-  // const user = session?.user;
+  const user = session?.user as User;
 
   // ✅ヘッダーアイコン画像の表示が遅いので、useSession()ではなく
   // getServerSession()を使ってサーバーサイドでセッション情報を取得するように変更
   // これにより、ヘッダーアイコン画像の表示が速くなる
   // ------------------
-  const session = await getServerSession(nextAuthOptions)
+  // const session = await getServerSession(nextAuthOptions)
   // これは現在NextAuthでログインしている人のユーザー情報（セッション情報）を取得している
-  const user = session?.user as User;
+  // const user = session?.user as User;
   // ------------------
 
 
@@ -49,11 +49,11 @@ const Header = async () => {
             ホーム
           </Link>
           <Link
-            href={user ? `/profile` : `/api/auth/signin`}
+            // href={user ? `/profile` : `/api/auth/signin`}
             // 上記NextAuthデフォルトのサインイン画面
             // 下記作成したサインイン画面どちらを使用しても良い
             // vercelにデプロイするときは上記の方が都合が良い
-            // href={user ? `/profile` : `/login`}
+            href={user ? `/profile` : `/login`}
             className="text-gray-300 hover:text-white px-3 py-2 rounded-md text-sm font-medium"
           >
             {user ? "プロフィール" : "ログイン"}
@@ -61,11 +61,11 @@ const Header = async () => {
 
 
           {user ?
-            // <button onClick={() => signOut({ callbackUrl: '/login' })}
-            //   className="text-gray-300 hover:text-white px-3 py-2 rounded-md text-sm font-medium">ログアウト</button> : ""
+            <button onClick={() => signOut({ callbackUrl: '/login' })}
+              className="text-gray-300 hover:text-white px-3 py-2 rounded-md text-sm font-medium">ログアウト</button> : ""
             // ✅onClickはクライアントコンポーネントでしか使えないので、下記に変更
             // /api/auth/signout" は NextAuth がデフォルトで用意しているサインアウト用エンドポイント
-            <Link href="api/auth/signout" className="text-gray-300 hover:text-white px-3 py-2 rounded-md text-sm font-medium">ログアウト</Link> : ""
+            // <Link href="api/auth/signout" className="text-gray-300 hover:text-white px-3 py-2 rounded-md text-sm font-medium">ログアウト</Link> : ""
           }
           {/* signOut関数はNextAuthが準備している関数で、ユーザーをログアウトさせるために使用し、callbackUrlオプションを指定することで、ログアウト後に遷移するURLを設定できます。 */}
 
