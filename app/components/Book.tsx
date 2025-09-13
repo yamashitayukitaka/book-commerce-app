78407215c4bbb992d9a933455ab755187f8d69d3
YUKITAKA YAMASHITA <takasnowstyle1821@gmail.com>
2025-09-14 01:49:00 +0900

    トップページのサムネイルから商品詳細ページへ遷移できるように実装

--- app/components/Book.tsx
+++ app/components/Book.tsx
@@ -54,18 +54,21 @@ const Book = ({ book, isPurchased }: BookProps) => {
     }
   };
 
+  const GoToProductDetail = () => {
+    router.push(`/book/${book.id}`);
+  }
 
   const handleCancel = () => {
     setShowModal(false);
   }
 
-  const handlePurchaseClick = () => {
-    if (isPurchased) {
-      alert('その商品は購入済みです');
-    } else {
-      setShowModal(true);
-    }
-  }
+  // const handlePurchaseClick = () => {
+  //   if (isPurchased) {
+  //     alert('その商品は購入済みです');
+  //   } else {
+  //     setShowModal(true);
+  //   }
+  // }
 
   const handlePurchaseConfirm = () => {
     if (!user) {
@@ -97,7 +100,7 @@ const Book = ({ book, isPurchased }: BookProps) => {
 
 
       <div className="flex flex-col items-center m-4">
-        <a onClick={handlePurchaseClick} className="cursor-pointer shadow-2xl duration-300 hover:translate-y-1 hover:shadow-none">
+        <a onClick={GoToProductDetail} className="cursor-pointer shadow-2xl duration-300 hover:translate-y-1 hover:shadow-none">
           <Image
             priority
             src={book.thumbnail.url}
@@ -112,19 +115,6 @@ const Book = ({ book, isPurchased }: BookProps) => {
             <p className="mt-2 text-md text-slate-700">値段：{book.price}円</p>
           </div>
         </a>
-        {showModal && (
-          <div className="absolute top-0 left-0 right-0 bottom-0 bg-slate-900 bg-opacity-50 flex justify-center items-center modal">
-            <div className="bg-white p-8 rounded-lg">
-              <h3 className="text-xl mb-4">本を購入しますか？</h3>
-              <button onClick={handlePurchaseConfirm} className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mr-4">
-                購入する
-              </button>
-              <button onClick={handleCancel} className="bg-gray-500 hover:bg-gray-700 text-white font-bold py-2 px-4 rounded">
-                キャンセル
-              </button>
-            </div>
-          </div>
-        )}
       </div>
     </>
   );
