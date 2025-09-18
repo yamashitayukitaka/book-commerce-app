6c0388b5aac9ed42c18ffceb6250eaf6b21ddbed
YUKITAKA YAMASHITA <takasnowstyle1821@gmail.com>
2025-09-18 10:09:28 +0900

    route.tsではHTTPメソッドのみexport可能であることをコメント追加

--- app/api/comments/[productId]/[commentId]/route.ts
+++ app/api/comments/[productId]/[commentId]/route.ts
@@ -2,6 +2,30 @@ import { PrismaClient } from '@prisma/client';
 import { NextResponse } from 'next/server';
 const prisma = new PrismaClient();
 
+// -------------------------------------------------------------------
+// ✅下記はvercelデプロイ時エラーになる
+// Next.js の app/api/.../route.ts では、
+// GET
+// POST
+// PUT
+// DELETE
+// のような HTTP メソッドだけが export できます。
+// （main は Next.js にとって不正な export だから弾かれています）
+
+
+// export async function main() {
+//   try {
+//     await prisma.$connect();
+//     // prisma.$connect()はDBとの接続を確立するためのメソッドです。
+//     // GETの場合は不要の場合があるが、POSTやPUTなどのデータを変更する操作を行う場合には、接続を確立しておく必要があります。
+//     // $connect() は 非同期メソッド であり、Prisma が内部で Promise を返すように作られています。
+//     // だから await が使えるのです。
+//   } catch (err) {
+//     return Error('DB接続に失敗しました')
+//   }
+// }
+// -------------------------------------------------------------------
+
 // 削除用
 export const DELETE = async (
   req: Request,
@@ -9,6 +33,7 @@ export const DELETE = async (
 ) => {
   try {
     // DB接続
+    // await main();間違いこのような方法はとらないこと
     await prisma.$connect();
     // commentId を使って削除
     const comment = await prisma.comment.delete({
