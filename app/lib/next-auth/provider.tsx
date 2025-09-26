74f2bf6bccbd1acffffbfef2dc3d7f5e8d740431
YUKITAKA YAMASHITA <takasnowstyle1821@gmail.com>
2025-09-26 17:16:38 +0900

    provider.tsxのコメントアウト削除

--- app/lib/next-auth/provider.tsx
+++ app/lib/next-auth/provider.tsx
@@ -1,8 +1,9 @@
 'use client'
-// import { SessionProvider } from "next-auth/react"
-// import React, { FC, PropsWithChildren } from "react";
+import { SessionProvider } from "next-auth/react"
+import React, { FC, PropsWithChildren } from "react";
 
 
-// export const NextAuthProvider: FC<PropsWithChildren> = ({ children }) => {
-//   return <SessionProvider>{children}</SessionProvider>
-// }
+export const NextAuthProvider: FC<PropsWithChildren> = ({ children }) => {
+  return <SessionProvider>{children}</SessionProvider>
+}
+
